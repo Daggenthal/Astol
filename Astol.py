06622f2e903a7b8422c662e3ad07b322762c1cd5
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2021-05-13 06:25:13 -0500

    Update Astol.py
    Fixed issue of not being able to read the string output of "subprocess.getoutput()" on different Distros

--- Astol.py
+++ Astol.py
@@ -1,7 +1,7 @@
 #Author:	Daggenthal
 #Started:	05/13/2021 at 01:20
 #Finished:	05/13/2021 at 03:23
-#Edited:	05/13/2021 at 04:55 (Added option to remove Nouveau for installation of NVIDIA drivers)
+#Edited:	05/13/2021 at 06:24 (Fixed issue of not being able to read the string output of "subprocess.getoutput()" on different Distros)
 
 
 # I created this as a tool to combine my Update.sh, and Software_Install.sh bash scripts. This was both to learn Python3, and to get rid of a shell-requirement, which this took me around 2 hours to learn how to do.
@@ -9,33 +9,35 @@
 # This tooks me lots of Googling. I learned about sys, subprocess, and platform to grab the things that I need. While I can read this and know what it does;
 # I need to re-do it a few more times to burn it into memory.
 
-import sys, subprocess							# This imports the required utilities for Python 3 to run this script.
+import sys, subprocess						# This imports the required utilities for Python 3 to run this script.
 
 while True:								# The program starts out with a loop so it doesn't exit / crash instantly when a non-supported string (input) is, well, inputted.
 	def update():							# Defines a function called update.
 	
-		OS = subprocess.getoutput(['cat /etc/os-release'])	# Defines OS, then runs a command located in (['']) + grabs the output of said command and stores it into the variable called OS.
-	
-		if OS.find('debian'):					# Scans the string that's outputted by the command and searches for what's in '', so in this case it's searching for the word "debian".
+		OS = subprocess.getoutput(['cat /etc/os-release'])	# Defines OS, then runs a command located in (['']) + grabs the output of said command and stores it into the variable called
+		#output, _ = OS.communicate()
+		#p_status = OS.wait()
+		 	
+		if 'debian' in OS:					# Scans the string that's outputted by the command and searches for what's in '', so in this case it's searching for the word "debian".
 			subprocess.run(['sudo apt update -y && sudo apt upgrade -y --allow-downgrades && sudo apt autoremove -y'], shell=True)	
-		elif OS.find('fedora'):
+		elif 'fedora' in OS:
 			subprocess.run(['sudo dnf update -y && sudo dnf upgrade -y'], shell=True)
-		elif OS.find('arch'):
+		elif 'arch' in OS:
 			subprocess.run(['sudo pacman -Syu'], shell=True) # Runs a command through the shell based off of what is returned by the string. In this instance, it runs "sudo pacman -Syu" because the scanned string contained the word "arch" in it.
-		elif OS.find('opensuse'):
+		elif 'opensuse' in OS:
 			subprocess.run(['sudo zypper refresh && sudo zypper update -y'], shell=True)
 
 	def software():
 	
 		OS = subprocess.getoutput(['cat /etc/os-release'])
 		
-		if OS.find("debian"):
+		if 'debian' in OS:
 			subprocess.run(['clear && printf "Please input the software you would like to install: " && read install && sudo apt install -y $install'], shell=True)
-		elif OS.find('fedora'):
+		elif 'fedora' in OS:
 			subprocess.run(['clear && printf "Please input the software you would like to install: " && read install && sudo dnf install -y $install'], shell=True)
-		elif OS.find('arch'):
+		elif 'arch' in OS:
 			subprocess.run(['clear && printf "Please input the software you would like to install: " && read install && sudo pacman -S --noconfirm $install'], shell=True)
-		elif OS.find('opensuse'):
+		elif 'opensuse' in OS:
 			subprocess.run(['clear && printf "Please input the software you would like to install: " && read install && sudo zypper install -y $install'], shell=True)
 	
 	def nvidia():
