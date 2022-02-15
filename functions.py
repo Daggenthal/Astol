9bbbf9c662f25e5410b11aab6d7ffd23c19859b6
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2022-02-14 19:18:32 -0600

    Removed 'ubuntu' in the '/etc/os-release' search

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def update():								# Defines a function called update.
 		
 	OS = subprocess.getoutput(['cat /etc/os-release'])		# Defines OS, then runs a command located in (['']) + grabs the output of said command and stores it into the variable called
 
-	if 'debian' 'ubuntu' in OS:					# Scans the string that's outputted by the command and searches for what's in '', so in this case it's searching for the word "debian".
+	if 'debian' in OS:					# Scans the string that's outputted by the command and searches for what's in '', so in this case it's searching for the word "debian".
 		subprocess.run(['sudo apt update -y && sudo apt upgrade -y --allow-downgrades && sudo apt autoremove -y'], shell=True)	
 	elif 'fedora' in OS:
 		subprocess.run(['sudo dnf update -y && sudo dnf upgrade -y'], shell=True)
@@ -32,7 +32,7 @@ def remove():
 		if response == '1':
 
 			OS = subprocess.getoutput(['cat /etc/os-release'])
-			if 'debian' 'ubuntu' in OS:
+			if 'debian' in OS:
 				subprocess.run(['clear && printf "\t Please input the software you would like to remove: " && read remove && sudo apt remove $remove'], shell=True)
 			elif 'fedora' in OS:
 				subprocess.run(['clear && printf "\t Please input the software you would like to remove: " && read remove && sudo dnf remove $remove'], shell=True)
@@ -55,7 +55,7 @@ def software():
 		
 			OS = subprocess.getoutput(['cat /etc/os-release'])
 					
-			if 'debian' 'ubuntu' in OS:
+			if 'debian' in OS:
 				subprocess.run(['clear && printf "\t Please input the software you would like to install: " && read install && sudo apt install -y $install'], shell=True)
 			elif 'fedora' in OS:
 				subprocess.run(['clear && printf "\t Please input the software you would like to install: " && read install && sudo dnf install -y $install'], shell=True)
